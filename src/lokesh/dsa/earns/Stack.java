151f15d5b54240a5fff60a6bc7b9768ad62973da
lokesh-palieni <lokesh951555@gmail.com>
2023-04-29 14:50:52 +0530

    Abstract stack

---
+++ src/lokesh/dsa/earns/Stack.java
@@ -0,0 +1,16 @@
+package lokesh.dsa.earns;
+
+public class Stack {
+	private int size;
+	
+	public void push(int i) {
+		
+	}
+	
+	public int pop() {
+		if(size == 0) {
+			return -1;
+		}
+		else return 0;
+	}
+}
