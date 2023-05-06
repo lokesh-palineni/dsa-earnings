a4e53d3161a02708688b5091a5d52c9756700ebd
lokesh-palieni <lokesh951555@gmail.com>
2023-05-06 12:13:40 +0530

    BitWise Operators

---
+++ src/dsa/bitwise/BitWiseOperators.java
@@ -0,0 +1,38 @@
+package dsa.bitwise;
+
+import java.util.Scanner;
+
+public class BitWiseOperators {
+
+	static void bitwiseAND(int x,int y) {
+		System.out.println(x&y);
+	}
+	static void bitwiseOR(int x,int y) {
+		System.out.println(x&y);
+	}
+	static void bitwiseXOR(int x,int y) {
+		System.out.println(x&y);
+	}
+	static void bitwiseNOT(int x,int y) {
+		System.out.println(x&y);
+	}
+	static void bitwiseLeftShift(int x,int y) {
+		System.out.println(x&y);
+	}
+	static void bitwiseRightShift(int x,int y) {
+		System.out.println(x&y);
+	}
+	static void bitwiseUnSignedRightShift(int x,int y) {
+		System.out.println(x&y);
+	}
+	public static void main(String[] args) {
+ 
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		bitwiseAND(n,m);
+		
+		sc.close();
+	}
+
+}
