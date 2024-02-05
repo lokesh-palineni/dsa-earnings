5257ff6e881dd69b66d72c737f42e5d9f1b27202
lokesh-palieni <lokesh951555@gmail.com>
2024-02-05 12:38:58 +0530

    graph using arraylist

---
+++ src/dsa/graphs/GraphEx.java
@@ -0,0 +1,39 @@
+package dsa.graphs;
+
+import java.util.ArrayList;
+
+public class GraphEx {
+
+	public static void main(String[] args) {
+
+		int v = 5;
+		ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(v);
+		for (int i = 0; i < v; i++) {
+			adj.add(new ArrayList<Integer>());
+		}
+
+		addEdge(adj, 0, 1);
+		addEdge(adj, 0, 2);
+		addEdge(adj, 1, 2);
+		addEdge(adj, 1, 3);
+
+		PrintGraphics(adj);
+	}
+
+	static void PrintGraphics(ArrayList<ArrayList<Integer>> adj) {
+		for (int i = 0; i < adj.size(); i++) {
+			for (int j = 0; j < adj.get(i).size(); j++) {
+				System.out.print(adj.get(i).get(j) + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+	static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v) {
+
+		adj.get(u).add(v);
+		adj.get(v).add(u);
+	}
+
+}
